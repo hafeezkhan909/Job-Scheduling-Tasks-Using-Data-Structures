aa764c3fd9cbf40f835cc373daeedc621b5f7133
Mohammed Abdul Hafeez Khan <82383549+hafeezkhan909@users.noreply.github.com>
2022-05-05 09:29:51 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pcb.h"
+#include "data.h"
+#include "queue.h"
+#include "sjf.h"
+#include "rr.h"
+#include "fcfs.h"
+
+int main()
+{
+	FILE* out;
+	Data* data;
+
+	out = fopen("processes.out", "w");
+	data = parseFile("processes.in");
+
+	if (strcmp(data->algorithm, "sjf") == 0)
+	{
+		shortestJobFirst(data, out);
+	}
+	else if (strcmp(data->algorithm, "rr") == 0)
+	{
+		rr(data, out);
+	}
+	else if (strcmp(data->algorithm, "fcfs") == 0)
+	{
+		firstComeFirstServed(data, out);
+	}
+
+	destroyData(data);
+	fclose(out);
+	return 0;
+}
